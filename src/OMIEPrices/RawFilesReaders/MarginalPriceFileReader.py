822b315c35770a9c6f19c971ca09b9baf9fb4d99
acruzgarcia <a.cruz.garcia@gmail.com>
2020-11-12 21:02:37 +0000

    Added MarginalPriceFileReader

--- src/OMIEPrices/RawFilesReaders/MarginalPriceFileReader.py
+++ src/OMIEPrices/RawFilesReaders/MarginalPriceFileReader.py
@@ -8,17 +8,20 @@ class MarginalPriceFileReader:
     filename: str
 
     # Private variables
-    __str_line_price_old = 'Precio marginal (Cent/kWh)'
-    __str_line_price_new_spain = 'Precio marginal en el sistema español (EUR/MWh)'
-    __str_line_price_new_pt = 'Precio marginal en el sistema portugués (EUR/MWh)'
+    __str_line_price_old__cE__ = 'Precio marginal (Cent/kWh)'
+    __str_line_price_old__E__ = 'Precio marginal (EUR/MWh)'
+    __str_line_price_new_spain_cE__ = 'Precio marginal en el sistema español (Cent/kWh)'
+    __str_line_price_new_spain_E__ = 'Precio marginal en el sistema español (EUR/MWh)'
+    __str_line_price_new_pt__cE__ = 'Precio marginal en el sistema portugués (Cent/kWh)'
+    __str_line_price_new_pt__E__ = 'Precio marginal en el sistema portugués (EUR/MWh)'
 
-    __key_list_table = ['DATE','COUNTRY',
+    __key_list_table__ = ['DATE', 'COUNTRY',
                       'H1', 'H2','H1', 'H2','H3', 'H4','H5', 'H6','H7', 'H8','H9', 'H10',
                       'H11', 'H12','H13', 'H14','H15', 'H16','H17', 'H18','H19', 'H20',
                       'H21', 'H22','H23', 'H24']
 
-    __dateFormatInFile = '%d/%m/%Y'
-    __localeInFile = "en_DK.UTF-8"
+    __dateFormatInFile__ = '%d/%m/%Y'
+    __localeInFile__ = "en_DK.UTF-8"
 
     ####################################################################################################################
     def __init__(self, filename: str):
@@ -26,44 +29,56 @@ class MarginalPriceFileReader:
     ####################################################################################################################
 
     ####################################################################################################################
-    def readPricesFile(self):
+    def getPriceKeys(self, filename: str):
+        return self.__key_list_table__
+
+    ####################################################################################################################
+
+    ####################################################################################################################
+    def pricesGenerator(self):
 
         # Method yield each dictionary one by one
-        with open(self.filename, 'r') as file:
+        file = open(self.filename, 'r')
 
-            # from first line we get the units and the price date. We just look at the date
-            line = file.readline()
-            matches = re.findall('\d\d/\d\d/\d\d\d\d', line)
-            if not (len(matches) == 2):
-                print('File ' + self.filename + ' does not have the expected format.')
-            else:
-                # The second date is the one we want
-                dateprice = dt.datetime.strptime(matches[1],self.__dateFormatInFile)
+        # from first line we get the units and the price date. We just look at the date
+        line = file.readline()
+        matches = re.findall('\d\d/\d\d/\d\d\d\d', line)
+        if not (len(matches) == 2):
+            print('File ' + self.filename + ' does not have the expected format.')
+        else:
+            # The second date is the one we want
+            dateprice = dt.datetime.strptime(matches[1], self.__dateFormatInFile__)
 
-                # Process all the lines
-                while line:
-                    # read following line
-                    line = file.readline()
-                    splits = line.split(sep=';')
-                    if splits[0] == self.__str_line_price_old:
-                        yield self.__processLine__(dateprice=dateprice, country='ESP', line=line, multiplier = 10.0)
-                    elif splits[0] == self.__str_line_price_new_spain:
-                        yield self.__processLine__(dateprice=dateprice, country='ESP', line=line, multiplier=1.0)
-                    elif splits[0] == self.__str_line_price_new_pt:
-                        yield self.__processLine__(dateprice=dateprice, country='PT', line=line, multiplier=1.0)
+            # Process all the lines
+            while line:
+                # read following line
+                line = file.readline()
+                splits = line.split(sep=';')
+                if splits[0] == self.__str_line_price_old__cE__:
+                    yield self.__processLine__(dateprice=dateprice, country='ESP', line=line, multiplier=10.0)
+                if splits[0] == self.__str_line_price_old__E__:
+                    yield self.__processLine__(dateprice=dateprice, country='ESP', line=line, multiplier=1.0)
+                elif splits[0] == self.__str_line_price_new_spain_cE__:
+                    yield self.__processLine__(dateprice=dateprice, country='ESP', line=line, multiplier=10.0)
+                elif splits[0] == self.__str_line_price_new_spain_E__:
+                    yield self.__processLine__(dateprice=dateprice, country='ESP', line=line, multiplier=1.0)
+                elif splits[0] == self.__str_line_price_new_pt__cE__:
+                    yield self.__processLine__(dateprice=dateprice, country='PT', line=line, multiplier=10.0)
+                elif splits[0] == self.__str_line_price_new_pt__E__:
+                    yield self.__processLine__(dateprice=dateprice, country='PT', line=line, multiplier=1.0)
 
     ####################################################################################################################
 
     ####################################################################################################################
     def __processLine__(self, dateprice: dt.datetime, country: str, line: str, multiplier = 1.0) -> dict:
 
-        result = dict.fromkeys(self.__key_list_table)
+        result = dict.fromkeys(self.__key_list_table__)
 
         splits = line.split(sep=';')
         result['DATE'] = dateprice
         result['COUNTRY'] = country
 
-        locale.setlocale(locale.LC_NUMERIC, self.__localeInFile)
+        locale.setlocale(locale.LC_NUMERIC, self.__localeInFile__)
         for i in range(1, 25):
             result['H' + f'{i:01}'] = multiplier * locale.atof(splits[i])
 
--- src/OMIEPrices/TestingRawFilesReader/TestingMarginalPricesReader.py
+++ src/OMIEPrices/TestingRawFilesReader/TestingMarginalPricesReader.py
@@ -4,18 +4,29 @@ import os
 import filecmp
 
 ########################################################################################################################
-def Test1():
+def AllKeysInDictionary():
 
-    folderOut = os.path.abspath('OutputTesting')
-    filename = os.path.join(folderOut, 'PMD_20090601.txt')
+    folder = os.path.abspath('InputTesting')
+    filename = os.path.join(folder,'PMD_20060101.txt')
     reader = MarginalPriceFileReader(filename=filename)
 
-    out = reader.readPricesFile()
+    prices = list(reader.pricesGenerator())
+
+    keys = ['DATE', 'COUNTRY',
+            'H1', 'H2', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'H10',
+            'H11', 'H12', 'H13', 'H14', 'H15', 'H16', 'H17', 'H18', 'H19', 'H20',
+            'H21', 'H22', 'H23', 'H24']
+
+    for dictionary in prices:
+        thekeys = list(dictionary.keys())
+        for k in keys:
+            assert dictionary[k], 'Key: ' + k + ' not found.'
+
 ########################################################################################################################
 
 # Unoffical testing ....
 if __name__ == '__main__':
 
     # run the tests, they will fill if they do not pass
-    Test1()
+    AllKeysInDictionary()
     print('Test1() passed.')
