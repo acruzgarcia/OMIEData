aafbf361d2acd7eeec154099b7ba8b1029398d4a
Mirel Mora <mirel.mora@gmail.com>
2020-11-13 17:38:19 +0000

    Add System Types in EnergyByTechnology

--- src/OMIEPrices/Testing/TestingDownloaders/TestEnergyByTechnologyDownloader.py
+++ src/OMIEPrices/Testing/TestingDownloaders/TestEnergyByTechnologyDownloader.py
@@ -4,16 +4,18 @@ import os
 import filecmp
 
 from Downloaders.EnergyByTechnologyDownloader import EnergyByTechnologyDownloader
-from Downloaders.MarginalPriceDownloader import MarginalPriceDownloader
+from Downloaders.EnergyByTechnologyDownloader import SystemType
 
 ########################################################################################################################
 def Test1():
 
     folder = os.path.abspath('OutputTesting')
-    reader = EnergyByTechnologyDownloader(output_folder=folder)
+    downloader = EnergyByTechnologyDownloader(system=SystemType.IBERIAN, output_folder=folder)
 
-    assert reader.getCompleteURL() == \
-           'https://www.omie.es/sites/default/files/dados/AGNO_YYYY/MES_MM/TXT/INT_PBC_TECNOLOGIAS_H_9_DD_MM_YYYY_DD_MM_YYYY.TXT'
+    assert downloader.getCompleteURL() == \
+           'https://www.omie.es/sites/default/files/dados/AGNO_YYYY/MES_MM/TXT/INT_PBC_TECNOLOGIAS_H_9_DD_MM_YYYY_DD_MM_YYYY.TXT', \
+        'URL mask is not the one expected.'
+
 ########################################################################################################################
 
 ########################################################################################################################
@@ -22,13 +24,14 @@ def Test2():
     dateIni = dt.datetime(2020, 11, 13)
     dateEnd = dt.datetime(2020, 11, 13)
     folderOut = os.path.abspath('OutputTesting')
-    downloader = EnergyByTechnologyDownloader(output_folder=folderOut)
+    # System 1= Spain 2= Portugal 9= Iberian Market
+    downloader = EnergyByTechnologyDownloader(system=SystemType.IBERIAN, output_folder=folderOut)
 
     error = downloader.downloadData(dateIni=dateIni, dateEnd=dateEnd)
     assert error == 0, 'There was an error when downloading.'
 
     # Check it downloaded with the right name
-    outputFileName = 'EnergyByTechnology_20201113.txt'
+    outputFileName = 'EnergyByTechnology_9_20201113.txt'
     assert os.path.isfile(os.path.join(folderOut, outputFileName)), \
         'The downloaded file does not have the expected name.'
 
