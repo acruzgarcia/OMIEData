518074b20648ebe4fec44c368100164de066c75f
acruzgarcia <a.cruz.garcia@gmail.com>
2022-03-23 16:21:25 +0100

    Updated example

--- examples/example_marginal_price_on_the_fly.py
+++ examples/example_marginal_price_on_the_fly.py
@@ -5,11 +5,24 @@ from OMIEData.DataImport.omie_marginalprice_importer import OMIEMarginalPriceFil
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
+    import matplotlib.pyplot as plt
+    from OMIEData.Enums.all_enums import DataTypeInMarginalPriceFile
+
     # download the files
-    dateIni = dt.datetime(2012, 3, 11)
-    dateEnd = dt.datetime(2012, 4, 15)
+    dateIni = dt.datetime(2020, 1, 1)
+    dateEnd = dt.datetime(2022, 3, 22)
 
     # This can take time, it is downloading the files from the website..
     df = OMIEMarginalPriceFileImporter(date_ini=dateIni, date_end=dateEnd).read_to_dataframe(verbose=True)
     df.sort_values(by='DATE', axis=0, inplace=True)
     print(df)
+
+    # Just spanish prices
+    str_price_spain = str(DataTypeInMarginalPriceFile.PRICE_SPAIN)
+    dfPrices = df[df.CONCEPT == str_price_spain]
+
+    plt.figure()
+    plt.plot(dfPrices.DATE, dfPrices.H12, label='H12')
+    plt.plot(dfPrices.DATE, dfPrices.H23, label='H23')
+    plt.legend()
+    plt.show()
